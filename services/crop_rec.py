0897e625f150e6ae827bbc05b9ac4aad5cfee787
Gyanendra Shukla <srirajshukla@gmail.com>
2021-10-30 03:37:00 +0530

    feat: add crop prediction model

--- services/crop_rec.py
+++ services/crop_rec.py
@@ -1,4 +1,14 @@
+import pickle as pkl
 
-def recommend_crop():
+def recommend_crop(nitrogen, phosphorus, potas, humidity, ph, rainfall, temp):
+    with open('model.pkl') as f:
+        model = pkl.load(f)
+    
+    # zip probability with name and return top 5
+
+    return model.predict([[nitrogen, phosphorus, potas, humidity, ph, rainfall, temp]])
+
+
+def send_subscription(task):
     pass
 
