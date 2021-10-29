a4fb08ed999aa20e6d69ce5e136bf57899f2217e
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-30 04:01:32 +0530

    refactor: extract env vars

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+env = environ.Env()
+environ.Env.read_env(path.join(BASE_DIR, '.env.development'))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)bbec!k!^z6ul))q##xjg)_w#&96%^in(36dbcj(&&zs67&2+)'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
@@ -196,8 +201,7 @@ Q_CLUSTER = {
     'cpu_affinity': 1,
     'label': 'Farmer Q',
     'redis': {
-        'host': '127.0.0.1',
-        'port': 6379,
-        'db': 0, 
+        'host': env.str('REDIS_URL'),
+        'port': env.str('REDIS_PORT'),
     }
 }
