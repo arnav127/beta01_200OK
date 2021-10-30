b5abb4816868d0c2b3eaa8801e1f0763b86502c6
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-31 01:52:16 +0530

    fix: convert harvested date into optional arg

--- crop_plantations/schema.py
+++ crop_plantations/schema.py
@@ -31,10 +31,13 @@ class CropPlantationCreate(graphene.Mutation):
     
     @classmethod
     @login_required
-    def mutate(self, root, info, crop_name, planted_date, harvested_date):
+    def mutate(self, root, info, crop_name, planted_date, **kwargs):
         crop = Crops.objects.get(name = crop_name)
         
-        crop_plantation = CropPlantation.objects.create(crop=crop, farmer=info.context.user, planted_date=planted_date, harvested_date=harvested_date)
+        crop_plantation = CropPlantation(crop=crop, farmer=info.context.user, planted_date=planted_date)
+        if kwargs["harvested_date"]:
+            crop_plantation.harvested_date = kwargs["harvested_date"]
+        crop_plantation.save()
         return CropPlantationCreate(crop_plantation = crop_plantation)
 
 class CropPlantationUpdate(graphene.Mutation):
