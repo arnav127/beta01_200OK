a2eecea80e3e6a2b0fa43c7197c7cb63f9cf0a9d
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-30 14:59:50 +0530

    feat: add subscriptions, channels, asgi

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django_filters',
     'django_q',
     'django_crontab',
+    'channels',
     'farmer',
     'crops',
     'crop_plantations',
@@ -86,6 +87,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+ASGI_APPLICATION = 'core.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -213,3 +215,4 @@ Q_CLUSTER = {
 CRONJOBS = [
     ('0 8 * * *', 'services.warnings.weather')
 ]
+
