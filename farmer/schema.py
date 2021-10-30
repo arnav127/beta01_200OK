f001ddf870576e34734ba6f46ec7544d05f00f68
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-30 11:58:24 +0530

    feat: add plantation update, delete methods

--- farmer/schema.py
+++ farmer/schema.py
@@ -103,9 +103,38 @@ class CropPlantationCreate(graphene.Mutation):
         crop_plantation, _ = CropPlantation.objects.create(crop=crop, farmer=info.context.user, planted_date=planted_date, harvested_date=harvested_date)
         return CropPlantationCreate(crop_plantation = crop_plantation)
 
+class CropPlantationUpdate(graphene.Mutation):
+
+    crop_plantation = graphene.Field(CropPlantationType)
+    class Arguments:
+        id = graphene.ID(required=True)
+        crop_name = graphene.String()
+        planted_date = graphene.Date()
+        harvested_date = graphene.Date()
+    
+    @classmethod
+    def mutate(self, root, info, **kwargs):
+        crop_plantation = CropPlantation.objects.get(pk=id, farmer = info.context.user)
+        for k, v in kwargs.items():
+            setattr(crop_plantation, k, v)
+        crop_plantation.sive()
+        return CropPlantationUpdate(crop_plantation = crop_plantation)
+
+class CropPlantationDelete(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        nd = CropPlantation.objects.get(pk=id, farmer = info.context.user).delete()
+        return CropPlantationDelete(success = (nd[0] == 1))
+
 class CropPlantationMutation(graphene.ObjectType):
     create_crop_plantation = CropPlantationCreate.Field()
-
+    update_crop_plantation = CropPlantationUpdate.Field()
+    delete_crop_plantation = CropPlantationDelete.Field()
 
 class SoilHealthMutation(graphene.ObjectType):
     pass
