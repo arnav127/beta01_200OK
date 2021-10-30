b3b9d0af607cfd0083025f5804ee47110af1f8f9
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-30 16:33:45 +0530

    fix: update soil health create fields

--- soil_health/models.py
+++ soil_health/models.py
@@ -4,11 +4,12 @@ from farmer.models import ExtendUser
 
 class SoilHealth(models.Model):
     owner = models.ForeignKey(ExtendUser, on_delete=CASCADE)
-    soil_type = models.CharField(blank=False, max_length=40)
-    soil_ph = models.FloatField(blank=False)
+    ph = models.FloatField(blank=False)
     nitrogen = models.FloatField(blank=False)
     phosphorus = models.FloatField(blank=False)
-    potassium = models.FloatField(blank=False)
+    potas = models.FloatField(blank=False)
+    temp = models.FloatField(blank=False)
+    rainfall = models.FloatField(blank=False)
 
     def __str__(self):
         return self.owner.username + " - " + self.soil_type
--- soil_health/schema.py
+++ soil_health/schema.py
@@ -11,27 +11,58 @@ class SoilHealthType(DjangoObjectType):
     class Meta:
         model = SoilHealth
 
+
 class SoilHealthQuery(graphene.ObjectType):
     pass
+
+
 class SoilHealthCreate(graphene.Mutation):
     soil_health = graphene.Field(SoilHealthType)
+
     class Arguments:
-        soil_type = graphene.String()
-        soil_ph = graphene.Float()
-        soil_nitrogen = graphene.Float()
-        soil_potassium = graphene.Float()
-        soil_phosphorus = graphene.Float()
-    
+        nitrogen = graphene.Decimal()
+        phosphorus = graphene.Decimal()
+        potas = graphene.Decimal()
+        humidity = graphene.Decimal()
+        ph = graphene.Decimal()
+        rainfall = graphene.Decimal()
+        temp = graphene.Decimal()
+
     @classmethod
-    def mutate(self, root, info,soil_type, soil_ph, soil_nitrogen, soil_potassium, soil_phosphorus):
-        soil_health = SoilHealth.objects.create(owner=info.context.user, soil_type=soil_type, 
-            soil_ph=soil_ph, soil_nitrogen=soil_nitrogen,
-            soil_potassium=soil_potassium, soil_phosphorus=soil_phosphorus)
-        return SoilHealthCreate(soil_health = soil_health)
+    def mutate(
+        self,
+        root,
+        info,
+        nitrogen, phosphorous, potas, humidity, ph, rainfall, temp
+    ):
+        soil_health = SoilHealth.objects.create(
+            owner=info.context.user,
+            ph=ph,
+            nitrogen=nitrogen,
+            potassium=potas,
+            phosphorus=phosphorous,
+            temp = temp,
+            rainfall = rainfall
+        )
+        async_task(
+            "services.crop_rec.recommend_crop",
+            nitrogen,
+            phosphorous,
+            potas,
+            humidity,
+            ph,
+            rainfall,
+            temp,
+            info.context.user.username,
+            hook="services.crop_rec.send_subscription",
+        )
+        return SoilHealthCreate(soil_health=soil_health)
+
 
 # sends a task to the q to predict
 class CheckSoilHealth(graphene.Mutation):
     success = graphene.Boolean()
+
     class Arguments:
         nitrogen = graphene.Decimal()
         phosphorus = graphene.Decimal()
@@ -43,10 +74,24 @@ class CheckSoilHealth(graphene.Mutation):
 
     @classmethod
     @login_required
-    def mutate(self, root, info, nitrogen, phosphorous, potas, humidity, ph, rainfall, temp):
+    def mutate(
+        self, root, info, nitrogen, phosphorous, potas, humidity, ph, rainfall, temp
+    ):
         # async_task()
-        async_task("services.crop_rec.recommend_crop", nitrogen, phosphorous, potas, humidity, ph, rainfall, temp, info.context.user.username, hook="services.crop_rec.send_subscription")
-        return CheckSoilHealth(success = True)
+        async_task(
+            "services.crop_rec.recommend_crop",
+            nitrogen,
+            phosphorous,
+            potas,
+            humidity,
+            ph,
+            rainfall,
+            temp,
+            info.context.user.username,
+            hook="services.crop_rec.send_subscription",
+        )
+        return CheckSoilHealth(success=True)
+
 
 # recieves the result from q and broadcasts the result
 class SendSoilHealth(graphene.Mutation):
@@ -58,13 +103,15 @@ class SendSoilHealth(graphene.Mutation):
 
     @classmethod
     def mutate(self, root, info, recs, group):
-        BroadcastSoilHealth.broadcast(group = group, payload=recs)
+        BroadcastSoilHealth.broadcast(group=group, payload=recs)
         return SendSoilHealth(success=True)
+
 
 class SoilHealthMutation(graphene.ObjectType):
     create_soil_health = SoilHealthCreate.Field()
     check_soil_health = CheckSoilHealth.Field()
     send_soil_health = SendSoilHealth.Field()
+
 
 # broadcast the result
 class BroadcastSoilHealth(channels_graphql_ws.Subscription):
@@ -82,4 +129,3 @@ class BroadcastSoilHealth(channels_graphql_ws.Subscription):
 
 class SoilHealthSubscriptions(graphene.ObjectType):
     soil_rec = BroadcastSoilHealth.Field()
-
