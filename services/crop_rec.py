ff1030cff2b73f75580e8cc0a59596faf8633a2d
Gyanendra Shukla <srirajshukla@gmail.com>
2021-10-30 17:12:59 +0530

    feat: crop recommend update

--- services/crop_rec.py
+++ services/crop_rec.py
@@ -1,15 +1,26 @@
 import pickle as pkl
 import requests
+import os
+
 
 def recommend_crop(nitrogen, phosphorus, potas, humidity, ph, rainfall, temp, group):
-    with open('model.pkl') as f:
+    p = os.path.dirname(os.path.abspath(__file__))
+    print(p)
+    ap = p + '\\model.pkl'
+    with open(ap, 'rb') as f:
+        print(ap)
+        print("h1")
         model = pkl.load(f)
+        print("h2")
     
     # zip probability with name and return top 5
-
+    print("here")
     probs =  model.predict_proba([[nitrogen, phosphorus, potas, humidity, ph, rainfall, temp]])
+    prediction_with_name = zip(model.classes_, probs)
+    prediction_with_name.sort(key=lambda x: x[1], reverse=True)
+    print(prediction_with_name)
 
-    return {probs, group}
+    return {prediction_with_name, group}
 
 
 def send_subscription(task):
@@ -34,3 +45,5 @@ def send_subscription(task):
     r = requests.post(url = url, json = data, headers={"content-type": "application/json"})
     print(r.text)
 
+
+print(recommend_crop(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 'group'))
