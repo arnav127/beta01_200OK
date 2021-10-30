48b5ed8ebadf2e8c4bc26cf19b0a2edc8e0690ec
Gyanendra Shukla <srirajshukla@gmail.com>
2021-10-31 05:13:57 +0530

    feat: add pwa

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import "./index.css";
 import { AuthProvider } from "./context/auth";
 
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 const httpLink = createHttpLink({
@@ -67,6 +68,8 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
+
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
