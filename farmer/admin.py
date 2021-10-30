62c16dccda4e0e4158b3f9a4079ce46df3931250
Gyanendra Shukla <srirajshukla@gmail.com>
2021-10-30 10:11:34 +0530

    feat: add soil model, add fields to other types
    1. added msrp to crops
    2. harvested date can be null now
    3. add soil type to crop plantation
    4. add str to all models

--- farmer/admin.py
+++ farmer/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import ExtendUser, Crops, CropPlantation
+from .models import ExtendUser, Crops, CropPlantation, SoilHealth
 # Register your models here.
 admin.site.register(ExtendUser)
 admin.site.register(Crops)
 admin.site.register(CropPlantation)
+
+@admin.register(SoilHealth)
+class SoilHealthAdmin(admin.ModelAdmin):
+    pass
--- farmer/models.py
+++ farmer/models.py
@@ -22,9 +22,32 @@ class Crops(models.Model):
     season = models.CharField(blank=False, max_length=40)
     photo = models.ImageField()
     about = models.TextField()
+    msrp = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
     
+
+class SoilHealth(models.Model):
+    owner = models.ForeignKey(ExtendUser, on_delete=CASCADE)
+    soil_type = models.CharField(blank=False, max_length=40)
+    soil_ph = models.FloatField(blank=False)
+    nitrogen = models.FloatField(blank=False)
+    phosphorus = models.FloatField(blank=False)
+    potassium = models.FloatField(blank=False)
+
+    def __str__(self):
+        return self.owner.username + " - " + self.soil_type
+
 class CropPlantation(models.Model):
     crop = models.ForeignKey(Crops, on_delete=CASCADE)
     farmer = models.ForeignKey(ExtendUser, on_delete=CASCADE)
+    soil_type = models.ForeignKey(SoilHealth, on_delete=CASCADE, blank=True, null=True)
     planted_date = models.DateField()
-    harvested_date = models.DateField()
+    harvested_date = models.DateField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.crop.name + " - " + self.farmer.username
+
+
