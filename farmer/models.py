27fe10638ede5731c883744f5cfc35ee66220eb6
Arnav Dixit <arnavdixit127@gmail.com>
2021-10-30 10:44:49 +0530

    fix: update image save location

--- farmer/models.py
+++ farmer/models.py
@@ -20,7 +20,7 @@ class Crops(models.Model):
 
     name = models.CharField(blank=False, max_length=40)
     season = models.CharField(blank=False, max_length=40)
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='images/crops/')
     about = models.TextField()
     msrp = models.DecimalField(max_digits=6, decimal_places=2)
 
