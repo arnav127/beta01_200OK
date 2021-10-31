457ae2e71dfac9077ad7c840ea9e4cabfd8fba7f
Seshal Jain <seshaljain11@gmail.com>
2021-10-31 11:58:14 +0530

    fix

--- src/components/Navigation/HomeBar/index.jsx
+++ src/components/Navigation/HomeBar/index.jsx
@@ -16,7 +16,7 @@ export default function Navigation() {
                             className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                             to="/"
                         >
-                            Farmer's Helpdesk
+                            FarmBuddy
                         </Link>
                         <button
                             className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
